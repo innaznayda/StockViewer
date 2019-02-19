dc5c3cae287e194bf0bfedac8ea932a49624054b
Inna Znaida <IZN@simcorp.com>
2019-02-19 14:54:07 +0200

    basic solution and its dependency

---
+++ StockViewer/ActorModel/ActorSystemReference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Akka.Actor;
+using Akka.DI.Core;
+using Akka.DI.Ninject;
+using Ninject;
+
+namespace StockViewer.ActorModel
+{
+    static class ActorSystemReference
+    {
+        public static ActorSystem ActorSystem { get; private set; }
+
+        static ActorSystemReference()
+        {
+            CreateActorSystem();
+        }
+
+        private static void CreateActorSystem()
+        {
+            ActorSystem = ActorSystem.Create("StockActorSystem");
+            var container = new StandardKernel();
+            IDependencyResolver resolver = new NinjectDependencyResolver(container, ActorSystem);
+        }
+    }
+}
