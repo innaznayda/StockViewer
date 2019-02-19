499adc59baa4f92323d7a535fc43e4504c9f53e9
Inna Znaida <IZN@simcorp.com>
2019-02-19 17:49:37 +0200

    basic solution and its dependency

---
+++ StockViewer/ActorModel/Messages/RefreshStockPriceMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace StockViewer.ActorModel.Messages
+{
+    class RefreshStockPriceMessage
+    {
+        public string StockSymbol { get; private set; }
+
+        public RefreshStockPriceMessage(string stock)
+        {
+            StockSymbol = stock;
+        }
+    }
+}
