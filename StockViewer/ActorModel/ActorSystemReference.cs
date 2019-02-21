815a6e10abcd4c6f153fcaf0d7ea79c7f64804f1
Inna Znaida <IZN@simcorp.com>
2019-02-21 16:55:09 +0200

    more logic

--- StockViewer/ActorModel/ActorSystemReference.cs
+++ StockViewer/ActorModel/ActorSystemReference.cs
@@ -3,6 +3,7 @@ using Akka.Actor;
 using Akka.DI.Core;
 using Akka.DI.Ninject;
 using Ninject;
+using StockViewer.ActorModel.Actors;
 using StockViewer.ExternalServices;
 
 namespace StockViewer.ActorModel
--- StockViewer/ActorModel/Actors/StockActor.cs
+++ StockViewer/ActorModel/Actors/StockActor.cs
@@ -1,20 +1,29 @@
 ﻿using Akka.Actor;
+using Akka.DI.Core;
 using StockViewer.ActorModel.Messages;
 using System.Collections.Generic;
 
 namespace StockViewer.ActorModel.Actors {
-    class StockActor : ReceiveActor{
+    class StockActor : ReceiveActor {
         private readonly string StockSymbol;
         private readonly HashSet<IActorRef> Subscribers;
-
+        private readonly IActorRef PriceLookupChild;
+        private decimal StockPrice;
 
         public StockActor(string stock) {
             StockSymbol = stock;
             Subscribers = new HashSet<IActorRef>();
+            PriceLookupChild = Context.ActorOf(Context.DI().Props<StockPriceLookupActor>());
             Receive<SubscibeToNewStockPriceMessage>(message=> Subscribers.Add(message.Subscriber));
             Receive<UnsibscibeFromNewStockPriceMessage>(message => Subscribers.Remove(message.Subscriber));
+            Receive<RefreshStockPriceMessage>(message => PriceLookupChild.Tell(message));
+            Receive<UpdatedStockPriceMessage>(message => {
+                StockPrice = message.Price;
+                var stockPriceMessage = new StockPriceMessage(StockSymbol, StockPrice, message.Date);
+                foreach (var subscriber in Subscribers) {
+                    subscriber.Tell(stockPriceMessage);
+                }
+            });
         }
-
-
     }
 }
