113a920bfc44bdfc2ecdc6da5ffd5014c0d09e2a
innaznayda <innaznayda@gmail.com>
2019-02-21 20:04:30 +0200

    more ui

---
+++ StockViewer/ViewModel/StoclToggleButtonViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StockViewer.ViewModel {
+    class StoclToggleButtonViewModel : ViewModelBase {
+        private string buttonText;
+        public string StockSymbol { get; set; }
+
+        public ICommand ToggleCommand { get; set; }
+
+        public IActorRef StockToggleButtonActorRef { get; private set; }
+
+       
+    }
+}
